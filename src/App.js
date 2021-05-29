89a56d61699c838029e8ba115d6f7683b565b234
kamal-choudhary <pakiboy.612@gmail.com>
2021-05-29 12:22:08 +0500

    made some changes in the app.js file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Cosmic from 'cosmicjs';
-import GlobalStyle from './components/GlobalStyle';
-import HomeContainer from './containers/HomeContainer';
+import React, { useState, useEffect, useRef } from "react";
+import Cosmic from "cosmicjs";
+import GlobalStyle from "./components/GlobalStyle";
+import HomeContainer from "./containers/HomeContainer";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-import Information from './components/Information';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Information from "./components/Information";
 
 function App() {
-
-  useEffect(()=> {
+  useEffect(() => {
     const script = document.createElement("script");
 
-    script.src = "https://cdn.jsdelivr.net/npm/chart.js@3.1.0/dist/chart.min.js";
+    script.src =
+      "https://cdn.jsdelivr.net/npm/chart.js@3.1.0/dist/chart.min.js";
     script.async = true;
 
     document.body.appendChild(script);
     console.log("APP STARTED");
-    console.log("MAPBOX VAR", process.env.MAPBOX_API_KEY);
-    console.log("env Object", process.env);
-
-  })
+  });
 
   return (
     <>
@@ -33,12 +26,9 @@ function App() {
         <Switch>
           <Route path="/" component={HomeContainer} />
         </Switch>
-        
       </Router>
     </>
-  )
-};
-
-
+  );
+}
 
 export default App;
